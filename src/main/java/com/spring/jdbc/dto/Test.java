20977e2b0437f9c9915558e26eee6b578f6f3d94
amitkumar24487024 <amitkumar2448@gmail.com>
2020-03-11 12:14:30 +0530

    Student DTO and Test Class created

---
+++ src/main/java/com/spring/jdbc/dto/Test.java
@@ -0,0 +1,11 @@
+package com.spring.jdbc.dto;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s=new Student(10,"Amit Singh","8884411410");
+		System.out.println(s);
+	}
+
+}
